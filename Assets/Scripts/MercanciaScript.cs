d17b30b1da7568353339ca08b6d97196c209801a
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2025-06-05 12:10:23 -0300

    MoneyManager

--- Assets/Scripts/MercanciaScript.cs
+++ Assets/Scripts/MercanciaScript.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class MercanciaScript : MonoBehaviour
 {
+    public float cost;
+    public moneyManager MoneyManager;
+
+     void Start()
+    {
+        MoneyManager = FindObjectOfType<moneyManager>();   
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
+            //actualizar dinero por la compra
+            MoneyManager.UpdateMoney(-cost);
             Destroy(gameObject);
         }
     }
